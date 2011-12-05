c636c40c02bc9d122506947ca672b5351a627dee
Christopher Lepore <christopher.r.lepore@gmail.com>
2011-12-04 20:13:28 -0500

    Added a cmd to remove the event incase the same element is used over and over

--- listDisplay.js
+++ listDisplay.js
@@ -6,7 +6,7 @@
 * A plugin to dynamically show a series of images with specific positioning
 *
 * @param options          An optional options object.
-* @param options.images   Array of objects to be positioned and shown (see example)
+* @param options.images   Array of images be positioned and shown (see example)
 * @param options.callBack A callback function to be called when the images are finished
 *
 **/
@@ -20,54 +20,72 @@
         var self = $(this);
 
         // Set options to be accesible across functions
-        self.data('listDisplay', { 
+        self.data('listDisplay', {
           options: options ,
           currentIndex: 0
         });
-        
+
         functions.setup.call(self);
       });
     }
   };
-      
+
   var functions = {
     setup: function() {
       var self = $(this),
        data = self.data('listDisplay');
-       
+
        // Create dynamic
        self.append($('<img class="dynamic" src="" />'));
        data.dynamic = self.find('.dynamic');
        data.dynamic.css('position', 'absolute');
-       
+
        // Set click
        self.bind('click', function() {
          functions.changeObject.call(self);
        });
-       
+
+       functions.preloadImages.call(self);
+
        data.currentIndex = 0;
        functions.changeObject.call(self);
     },
-  
+
     changeObject: function() {
       var self = $(this),
-       data = self.data('listDisplay');
-       
-       // Hide conatiner
-       if (data.currentIndex == data.options.images.length) {
-         data.dynamic.remove();
-         data.options.callBack.call(self);
-       } else {
-         // Set the attributes of the next image
-         data.dynamic.attr('src', data.options.images[data.currentIndex].src);
-         data.dynamic.css(data.options.images[data.currentIndex].position);
-       
+        data = self.data('listDisplay');
+
+      if (data.currentIndex == data.options.images.length) {
+        // Remove dynamic object and run the callback
+        data.dynamic.remove();
+        data.options.callBack.call(self);
+
+        // Remove click event
+        self.unbind('click');
+      } else {
+        // Set image position
+        data.dynamic.css(data.options.images[data.currentIndex].position);
+
+        // Set the image source to the next url
+        data.dynamic.attr('src', data.options.images[data.currentIndex].src);
+
         // Set the current index
         data.currentIndex++;
       }
+    },
+
+    preloadImages: function() {
+      var self = $(this),
+       data = self.data('listDisplay'),
+       tempImage = [];
+
+      $.each(data.options.images, function(i, item) {
+        tempImage[i] = new Image();
+        tempImage[i].src = item.src;
+      });
     }
   };
-    
+
   jQuery.fn.listDisplay = function(method) {
     // Method calling logic
     if (methods[method]) {
@@ -78,9 +96,10 @@
       $.error('Method ' +  method + ' does not exist');
     }
   };
-    
+
   jQuery.fn.listDisplay.defaults = {
     images: [],
     callBack: function(){ }
   };
 })(jQuery);
+
